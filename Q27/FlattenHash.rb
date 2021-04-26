bb5007095a4b7497c2752556f094319d8deca030
Rufus Tenali <rufustensky@gmail.com>
2021-04-26 15:19:23 +0100

    Create FlattenHash.rb

---
+++ Q27/FlattenHash.rb
@@ -0,0 +1,21 @@
+def flatten_hash(hash)
+    hash.each_with_object({}) do |(k, v), h|
+      if v.is_a? Hash
+        flatten_hash(v).map do |h_k, h_v|
+          h["#{k}.#{h_k}".to_sym] = h_v
+        end
+      else 
+        h[k] = v
+      end
+     end
+  end
+  
+  print flatten_hash({
+      "key": 3,
+      "foo": {
+          "a": 5,
+          "bar": {
+              "baz": 8
+          }
+      }
+  })
