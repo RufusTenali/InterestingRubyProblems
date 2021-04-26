5075eeaa5a01462bbaacca9b9d79dc6b433395c4
Rufus Tenali <rufustensky@gmail.com>
2021-04-26 17:14:53 +0100

    Create Non-DescendingArray.rb

---
+++ Q32/Non-DescendingArray.rb
@@ -0,0 +1,7 @@
+def check_possibility(nums)
+    num_diff = nums.each_cons(2).count {|x, y| (x - y) > 0}  
+    num_2_diff = nums.each_cons(3).count {|x, y, z| (x - z) > 0}  
+    result = (num_diff <= 1 && num_2_diff <= 1)  
+end
+
+p check_possibility([10, 5, 7])
