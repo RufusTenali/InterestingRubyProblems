63d59a974f5528c5d677a49b20d234f9e4faad6b
Rufus Tenali <rufustensky@gmail.com>
2021-05-05 12:09:58 +0100

    test file updated

--- T24/VisualBarChart_Test.rb
+++ T24/VisualBarChart_Test.rb
@@ -25,29 +25,3 @@ def largest_rectangle_area(array)
     end
     p biggest
 	end
-
-# largest_rectangle_area([1, 3, 2, 5])
-
-
-
-
-
-
-
-# def barchart(array)
-# 	index = 0
-# 	counter = array.size
-# 	until counter == 0
-# 		num = array[index]
-# 		if num > 0
-# 			print "x"
-# 			array[index] -= 1
-# 		else
-# 			index += 1
-# 			p "next"
-# 			counter -= 1
-# 		end
-# 	end
-# end
-
-# barchart([1, 3, 2, 5])
